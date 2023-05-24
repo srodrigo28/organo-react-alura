88edf3eb31cb30189c3fc3aae8761dfc664de810
Sebastiao <rodrigoexer1@gmail.com>
2023-05-23 21:04:06 -0300

    create-component-banner

---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import logo from './../../assets/banner.png';
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className="banner">
+            <img src={logo} />
+        </header>
+    )
+}
+export default Banner
